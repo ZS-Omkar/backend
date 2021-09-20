74802390cccd31ec1484be6b5cc0e66d922731ae
Omkar <omkar.betha@gmail.com>
2021-09-20 23:01:55 +0530

    ss

--- app/config/env.js
+++ app/config/env.js
@@ -1,9 +1,9 @@
 const env = {
-  database: 'loizenaidb',
-  username: 'postgres',
-  password: '123',
-  host: 'localhost',
-  dialect: 'postgres',
+  database: process.env.PGDATABASE,
+  username: process.env.PGUSER,
+  password: process.env.PGPASS,
+  host: process.env.PGHOST,
+  dialect: process.env.PGDIALECT,
   pool: {
     max: 5,
     min: 0,
